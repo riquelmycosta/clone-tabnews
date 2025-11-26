a48cb19607c07ab722eb39867eac5b1e6d4587ca
Riquelmy Costa <riquelmycosta@outlook.com>
2025-11-26 22:12:13 +0000

    altera a frase da pagina inicial

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Eu amo muito vocês, primas</h1>
+    return <h1>Eu amo muito vocês, primas. Sempre que se sentirem sozinhas, entre aqui</h1>
 }
 
 export default Home;
