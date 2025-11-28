87959900bb76e9bd4f699fabdf0949dc78c35cf1
Riquelmy Costa <riquelmycosta@outlook.com>
2025-11-28 00:09:41 +0000

    adciona scripts `lint:check` e `lint:write`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto em desenvolvimento
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home(){
-    return <h1>Eu amo muito vocês, primas. Sempre que se sentirem sozinhas, entre aqui</h1>
+function Home() {
+  return (
+    <h1>
+      Eu amo muito vocês, primas. Sempre que se sentirem sozinhas, entre aqui
+    </h1>
+  );
 }
 
 export default Home;
