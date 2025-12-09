c6c63e7da24f8075dc24fb77b4deba8f4c2ce266
Riquelmy Costa <riquelmycosta@outlook.com>
2025-12-09 00:53:05 +0000

    atualiza a pagina em construção

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,4 @@
 function Home() {
-  return (
-    <h1>
-      Eu amo muito vocês, primas. Sempre que se sentirem sozinhas, entre aqui
-    </h1>
-  );
+  return <h1>Pagina em construção...</h1>;
 }
-
 export default Home;
